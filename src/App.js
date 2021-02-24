433f233e0009d943c8b7309821a32bba6b97ea4c
Artem Sh <shulgaart274@gmail.com>
2021-02-24 17:50:43 +0300

    replace png on svg for checkbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { Switch, withRouter } from "react-router-dom";
 import Checkbox from "./checkbox/checkbox";
 
 function App() {
   return (
-    <Switch>
-      <>
-        <div className="app">
-          <Checkbox size={25} label="dark" color="dark" />
-          <Checkbox size={25} label="primary" color="primary" />
-          <Checkbox size={25} label="secondary" color="secondary" />
-        </div>
-      </>
-    </Switch>
+    <>
+      <div className="app">
+        <Checkbox size={25} label="dark" color="dark" />
+        <Checkbox size={25} label="primary" color="primary" />
+        <Checkbox size={25} label="secondary" color="secondary" />
+      </div>
+    </>
   );
 }
 
-export default withRouter(App);
+export default App;
